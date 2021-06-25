ec59bc1ff2b0c9a4d12734824db3dac8902be5f4
Jorge Manuel <jorgeyeez@gmail.com>
2021-06-25 14:32:08 +0100

    choose: Adição das Anotações das classes principais.

--- src/main/java/com/jorge/bookstore/domain/Livro.java
+++ src/main/java/com/jorge/bookstore/domain/Livro.java
@@ -1,12 +1,31 @@
 package com.jorge.bookstore.domain;
 
-public class Livro {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+
+@Entity
+public class Livro implements Serializable {
 	
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nome_autor;
 	private String texto;
 	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
