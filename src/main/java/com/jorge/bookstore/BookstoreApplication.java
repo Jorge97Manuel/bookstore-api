d34b3eba89a4f751accbfc897383d709d95eadb7
Jorge Manuel <jorgeyeez@gmail.com>
2021-06-25 15:21:04 +0100

    choose: Instancia automatica da Base de dados H2.

--- src/main/java/com/jorge/bookstore/BookstoreApplication.java
+++ src/main/java/com/jorge/bookstore/BookstoreApplication.java
@@ -1,13 +1,42 @@
 package com.jorge.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jorge.bookstore.domain.Categoria;
+import com.jorge.bookstore.domain.Livro;
+import com.jorge.bookstore.repositories.CategoriaRepository;
+import com.jorge.bookstore.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		
+		
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin","Lorem Y", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
 	}
 
 }
