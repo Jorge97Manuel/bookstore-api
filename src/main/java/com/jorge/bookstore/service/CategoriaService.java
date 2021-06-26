07bc1ea436ccd80f541dd606a962062dacbfe7ce
Jorge Manuel <jorgeyeez@gmail.com>
2021-06-26 20:39:21 +0100

    Choose: Criação da Exceptions para Mensagens de Erro.

--- src/main/java/com/jorge/bookstore/service/CategoriaService.java
+++ src/main/java/com/jorge/bookstore/service/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.jorge.bookstore.domain.Categoria;
 import com.jorge.bookstore.repositories.CategoriaRepository;
+import com.jorge.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objecto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
