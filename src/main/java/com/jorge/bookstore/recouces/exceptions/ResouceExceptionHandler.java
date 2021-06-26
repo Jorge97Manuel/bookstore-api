ec2aedba6bed3721e0cfc96b4633bf8be87240b7
Jorge Manuel <jorgeyeez@gmail.com>
2021-06-27 00:26:37 +0100

    Choose: Tratamento de Exception para o Delete.

--- src/main/java/com/jorge/bookstore/recouces/exceptions/ResouceExceptionHandler.java
+++ src/main/java/com/jorge/bookstore/recouces/exceptions/ResouceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.jorge.bookstore.service.exceptions.DataIntegrityViolationException;
 import com.jorge.bookstore.service.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -19,5 +20,14 @@ public class ResouceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 
 	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request) {
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+
+	}
+
 
 }
--- src/main/java/com/jorge/bookstore/service/CategoriaService.java
+++ src/main/java/com/jorge/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.jorge.bookstore.domain.Categoria;
@@ -22,11 +23,11 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objecto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
@@ -36,15 +37,17 @@ public class CategoriaService {
 		Categoria obj = findById(id);
 		obj.setNome(objDto.getNome());
 		obj.setDescricao(objDto.getDescricao());
-		
+
 		return repository.save(obj);
 	}
 
 	public void delete(Integer id) {
 		findById(id);
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.jorge.bookstore.service.exceptions.DataIntegrityViolationException("A Categoria não pode ser deletada! Possui livros associados");
+		}
 	}
-	
-	
 
 }
