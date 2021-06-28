bceac088562189d78fc9d735c0bcc4c95f39ed5a
Jorge Manuel <jorgeyeez@gmail.com>
2021-06-28 16:47:21 +0100

    Choose: Post para Livro.

--- src/main/java/com/jorge/bookstore/recouces/LivroResource.java
+++ src/main/java/com/jorge/bookstore/recouces/LivroResource.java
@@ -1,5 +1,6 @@
 package com.jorge.bookstore.recouces;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jorge.bookstore.domain.Livro;
 import com.jorge.bookstore.dtos.LivroDTO;
@@ -50,6 +53,14 @@ public class LivroResource {
 	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj){
 		Livro newObj = service.update(id, obj);
 		return ResponseEntity.ok().body(newObj);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat, @RequestBody Livro obj){
+		Livro newObj = service.create(id_cat, obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build(); 
 		
 	}
 }
--- src/main/java/com/jorge/bookstore/service/LivroService.java
+++ src/main/java/com/jorge/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jorge.bookstore.domain.Categoria;
 import com.jorge.bookstore.domain.Livro;
 import com.jorge.bookstore.repositories.LivroRepository;
 import com.jorge.bookstore.service.exceptions.ObjectNotFoundException;
@@ -44,6 +45,13 @@ public class LivroService {
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
 		
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 
 }
